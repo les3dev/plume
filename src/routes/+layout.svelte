97b604591f756395aa91618e88146a013fffbef2
Abdelcrks <abdelmoumenberkat@gmail.com>
2026-03-16 10:22:37 +0100

    feat: add new font dependencies and apply font styles to the application closes #14

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-  import { set_settings_context } from "$lib/settings/settings_context.svelte";
-	import { set_upload_context } from "$lib/upload/upload_context.svelte";
-    import "../app.css";
-    import {set_meeting_context} from "../lib/meeting/meeting_context.svelte";
+    import {set_settings_context} from '$lib/settings/settings_context.svelte';
+    import {set_upload_context} from '$lib/upload/upload_context.svelte';
+    import '../app.css';
+    import {set_meeting_context} from '../lib/meeting/meeting_context.svelte';
+    import '@fontsource/source-serif-pro';
+    import '@fontsource-variable/bricolage-grotesque';
 
     let {children} = $props();
-    set_settings_context()
+    set_settings_context();
     set_upload_context();
     set_meeting_context();
-
-
 </script>
 
 {@render children()}
